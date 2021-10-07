257d4a376b599e273297ac1a7d203dc16d0d1104
PaKonor <pakonorov89@gmail.com>
2021-10-08 00:29:55 +0300

    Добавил букву

--- 1_test/test_fix.py
+++ 1_test/test_fix.py
@@ -4,7 +4,7 @@
 def test_run(driver):
     driver.get("http://localhost/")
     print(driver.title)
-    assert driver.title  == "Your Sto" # убедимся, что находимся на той страгице, сравниваем title
+    assert driver.title  == "Your Store" # убедимся, что находимся на той страгице, сравниваем title
 
 # driver.title сравнение title (заголовок страници)
 # assert driver.title == "OTUS - Онлайн-образование"
